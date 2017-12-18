ecf29b913eaf79491e2cbcc747dde5de1b4a01dd
Chad Dewitt <cdewitt@transparent.com>
2017-12-18 15:34:58 -0500

    Add support for additional cpuacct cgroup directory.

--- src/psuedo_file_metrics.py
+++ src/psuedo_file_metrics.py
@@ -3,39 +3,42 @@ import json
 from os.path import join, isfile, isdir
 
 
-def parse_pseduo_dir(d):
+def parse_pseduo_dir(_directories):
     metrics = {}
-    if isdir(d):
-        for f in listdir(d):
-            if isfile(join(d, f)):
-                try:
-                    with open(join(d, f), 'r') as pseudo_file:
-                        metrics[f] = [l.replace('\n', '') for l in pseudo_file.readlines()]
-                except (IOError, OSError):
-                    pass
+    for d in _directories:
+        if isdir(d):
+            files = listdir(d)
+            for f in files:
+                if isfile(join(d, f)):
+                    try:
+                        with open(join(d, f), 'r') as pseudo_file:
+                            metrics[f] = [l.replace('\n', '') for l in pseudo_file.readlines()]
+                    except (IOError, OSError):
+                        pass
     return metrics
 
 
-def parse_net_dev(path):
+def parse_net_dev(paths):
     metrics = {}
-    if isfile(path):
-        try:
-            with open(path, 'r') as pseudo_file:
-                lines = pseudo_file.readlines()
-                if len(lines) >= 3:
-                    labels = [l.replace(' ', '').replace('\n', '') for l in lines[0].split('|') if l.replace(' ', '')]
-                    for i, label in enumerate(labels[1:], 1):
-                        metrics[label] = {}
-                        metric_names = lines[1].split('|')[i].split()
-                        interfaces = [il.split()[:1][0].replace(':', '') for il in lines[2:]]
-                        for interface_index, interface in enumerate(interfaces, 2):
-                            if i < 2:
-                                metric_values = lines[interface_index].split()[1:][:len(metric_names)]
-                            else:
-                                metric_values = lines[interface_index].split()[1:][len(metric_names):]
-                            metrics[label].update({interface: dict(zip(metric_names, metric_values))})
-        except (IOError, OSError):
-            pass
+    for path in paths:
+        if isfile(path):
+            try:
+                with open(path, 'r') as pseudo_file:
+                    lines = pseudo_file.readlines()
+                    if len(lines) >= 3:
+                        labels = [l.replace(' ', '').replace('\n', '') for l in lines[0].split('|') if l.replace(' ', '')]
+                        for i, label in enumerate(labels[1:], 1):
+                            metrics[label] = {}
+                            metric_names = lines[1].split('|')[i].split()
+                            interfaces = [il.split()[:1][0].replace(':', '') for il in lines[2:]]
+                            for interface_index, interface in enumerate(interfaces, 2):
+                                if i < 2:
+                                    metric_values = lines[interface_index].split()[1:][:len(metric_names)]
+                                else:
+                                    metric_values = lines[interface_index].split()[1:][len(metric_names):]
+                                metrics[label].update({interface: dict(zip(metric_names, metric_values))})
+            except (IOError, OSError):
+                pass
     return metrics
 
 
@@ -52,10 +55,15 @@ class PseudoFileStats(object):
             self.is_up = 0
 
     def get_psuedo_stat_dir(self, stat):
+        d = []
         if stat == 'net':
-            d = join(self.proc_dir, '{pid}/net/dev'.format(pid=self.pid))
+            d.append(join(self.proc_dir, '{pid}/net/dev'.format(pid=self.pid)))
+        elif stat == "cpu":
+            cpu_dir = join(self.cgroup_dir, '{stat}/docker/{cid}/'.format(stat=stat, cid=self.cid))
+            cpu_acct_dir = join(self.cgroup_dir, '{stat}/docker/{cid}/'.format(stat="cpuacct", cid=self.cid))
+            d.extend([cpu_dir, cpu_acct_dir])
         else:
-            d = join(self.cgroup_dir, '{stat}/docker/{cid}/'.format(stat=stat, cid=self.cid))
+            d.append(join(self.cgroup_dir, '{stat}/docker/{cid}/'.format(stat=stat, cid=self.cid)))
         return d
 
     def get_metrics(self):
--- tests.py
+++ tests.py
@@ -1,5 +1,10 @@
 import unittest
+from src.application import app
 
 
 class MetricsTestCase(unittest.TestCase):
-    pass
+    def test(self):
+        # TODO mock out cgroup files and add better assertions
+        self.client = app.test_client()
+        res = self.client.get("/metrics")
+        self.assertEqual(res.status_code, 200)
